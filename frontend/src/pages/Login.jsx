86f968f3fcf3df0992e841cab834a60b165c80d3
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-08-08 22:36:38 +0530

    updating .ignore

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from "react";
+
+function Login() {
+
+
+return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleLogin}>
+
+        
+      </form>
+    </div>
+  )
+}
+
+export default Login
