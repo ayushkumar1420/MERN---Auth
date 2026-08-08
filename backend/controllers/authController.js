c5dd5ea75dd45acb3d402dfd93d803954b7366b1
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-08-08 10:05:29 +0530

    worked on authcontroller to make user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,31 @@
 const User = require("../models/User.js");
 
 const signup = async (req, res) => {
-    res.send("signup api");
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if(existingUser){
+            return res.status(400).json({
+                message: "User already exists!",
+            });
+        }
+
+        const user = await User.create({
+            name, email, password,
+        });
+
+        res.status(201).json({
+            message: "User created succesfully",
+            user,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+        });
+    }
 };
 
 const login = async (req, res) => {
