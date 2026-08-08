6d097bb66585ef1313b11a23114790d4776ceddc
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-08-08 09:55:11 +0530

    added authRoutes and connected to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express()
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
+const authRoutes = require("./routes/authRoutes.js")
 
 dotenv.config();
 
 connectDB();
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("hello to new world")
